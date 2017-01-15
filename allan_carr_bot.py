42628f79268bf22d90c7585d23548a1a0282944c
vijai13 <vijay13@gmail.com>
2017-01-15 01:58:43 -0700

    Update allan_carr_bot.py

--- allan_carr_bot.py
+++ allan_carr_bot.py
@@ -3,22 +3,22 @@ import praw
 import re
 import random
 
-# Quotes taken from: http://www.imdb.com/character/ch0007553/quotes
-allan_quotes = \
+# Quotes taken from: https://en.wikipedia.org/wiki/Allen_Carr
+ac_quotes = \
 [
-" tbd ",
-" tbd 2",
+" smokers do not receive a boost from smoking a cigarette: smoking only relieves the withdrawal symptoms from the previous cigarette, which in turn creates more withdrawal symptoms once it is finished. In this way the drug addiction perpetuates itself. ",
+" The 'relief' smokers feel on lighting a cigarette, the feeling of being 'back to normal', is the feeling experienced by non-smokers all the time. So that smokers, when they light a cigarette are really trying to achieve a state that non-smokers enjoy their whole lives.",
 
 
 ]
 
-reddit = praw.Reddit('bot1')
+reddit = praw.Reddit('acbot')
 
-subreddit = reddit.subreddit("tbd")
+subreddit = reddit.subreddit("sandboxtest")
 
 for comment in subreddit.stream.comments():
     print(comment.body)
-    if re.search("quit", comment.body, re.IGNORECASE):
-            allan_reply = "Allan says: " + random.choice(allan_quotes)
-            comment.reply(allan_reply)
+    if re.search("cigarette", comment.body, re.IGNORECASE):
+            ac_reply = "According to this british author of books about quitting smoking: " + random.choice(ac_quotes)
+            comment.reply(ac_reply)
             print(all_reply)
