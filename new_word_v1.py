34288c8cf0412a3775e2796240aad61e113dd057
vijayalaya-k <vijay13@gmail.com>
2017-05-29 07:09:43 -0700

    Update new_word_v1.py

--- new_word_v1.py
+++ new_word_v1.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python
 import praw
+import json
 import re
 import random
 
 # observe and collect words typed up. identify new words, validate if they are present in the dictionary json and provide meaning...
 
+## read dictionay in Documents\Python\dictionary.json, read key and print value...
 
 
 
 # crutch below.. remove when the code is complete.
+
+   fruits = {
+        'apple':1,
+        'orange':2,
+        'banana':3
+    }
+
+	#if key 'apple' exists in fruits?
+    if 'apple' in fruits:
+        print(fruits['apple'])
+
+
 ac_quotes = \
 [
 " smokers do not receive a boost from smoking a cigarette: smoking only relieves the withdrawal symptoms from the previous cigarette, which in turn creates more withdrawal symptoms once it is finished. In this way the drug addiction perpetuates itself. ",
